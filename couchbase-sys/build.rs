e747b31a8bc6af1f86d5e9a7f1dc67e0fd2f0f4d
Michael Nitschinger <michael@nitschinger.at>
2017-01-30 07:24:15 +0100

    tighten up the build file for couchbase-sys

--- couchbase-sys/build.rs
+++ couchbase-sys/build.rs
@@ -8,28 +8,33 @@ use std::path::Path;
 fn main() {
     let lcb_dir = "libcouchbase-2.7.0";
     let out_dir = env::var("OUT_DIR").unwrap();
-    let target = env::var("TARGET").unwrap();
-    let windows = target.contains("windows");
+
+    // Step 1: Either use pkg-config or compile from source and link the library
+
+    let bindgen_path = if env::var("COUCHBASE_SYS_USE_PKG_CONFIG").is_ok() {
+        let result = pkg_config::find_library("libcouchbase").unwrap();
+        String::from(result.include_paths
+            .get(0)
+            .expect("Could not find include path from pkg config")
+            .to_str()
+            .unwrap())
+    } else {
+        let dst = cmake::build(lcb_dir);
+        println!("cargo:rustc-link-search=native={}",
+                 dst.join("lib").display());
+        println!("cargo:rustc-link-lib=dylib=couchbase");
+        format!("{}/include", out_dir)
+    };
+
+    // Step 2: From the headers, generate the rust binding via bindgen
 
     let _ = bindgen::builder()
-        .header(format!("{}/include/libcouchbase/couchbase.h", lcb_dir))
+        .header(format!("{}/libcouchbase/couchbase.h", bindgen_path))
+        .clang_arg("-I")
+        .clang_arg(bindgen_path)
         .no_unstable_rust()
+        .generate_comments(false)
         .generate()
         .unwrap()
         .write_to_file(Path::new(&out_dir).join("bindings.rs"));
-
-    if env::var("COUCHBASE_SYS_USE_PKG_CONFIG").is_ok() {
-        if pkg_config::find_library("libcouchbase").is_ok() {
-            return;
-        }
-    }
-
-    if windows {
-        panic!("Building from source for windows is not yet supported!");
-    }
-
-    let dst = cmake::build(lcb_dir);
-    println!("cargo:rustc-link-search=native={}",
-             dst.join("lib").display());
-    println!("cargo:rustc-link-lib=dylib=couchbase");
 }
