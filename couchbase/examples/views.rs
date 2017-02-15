e929ebc95e58da3984eede4ff5495b4afa2fb07d
Michael Nitschinger <michael@nitschinger.at>
2017-02-15 08:11:21 +0100

    fix views example

--- couchbase/examples/views.rs
+++ couchbase/examples/views.rs
@@ -14,7 +14,7 @@ fn main() {
     let bucket = cluster.open_bucket("beer-sample", "").expect("Could not open Bucket");
 
     // Run the query and iterate the rows.
-    for row in bucket.query_view("beer", "brewery_beers", "limit=-1").wait() {
+    for row in bucket.query_view("beer", "brewery_beers", "limit=1").wait() {
         match row {
             Ok(ViewResult::Row(r)) => println!("Found Row {:?}", r),
             Ok(ViewResult::Meta(m)) => println!("Found Meta {:?}", m),
