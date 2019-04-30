fa37fe13022ab5d7dadc94df5030fb2752484edb
Michael Nitschinger <michael@nitschinger.at>
2019-04-30 12:44:50 +0200

    speed up lcb builds

--- couchbase-sys/build.rs
+++ couchbase-sys/build.rs
@@ -7,11 +7,18 @@ fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     let mut build_cfg = cmake::Config::new("libcouchbase");
+
     if env::var("PROFILE").unwrap() == "release" {
         build_cfg.define("CMAKE_BUILD_TYPE", "RelWithDebInfo");
     } else {
         build_cfg.define("CMAKE_BUILD_TYPE", "DEBUG");
     }
+
+    build_cfg.define("LCB_NO_TESTS", "ON");
+    build_cfg.define("LCB_NO_TOOLS", "ON");
+    build_cfg.define("LCB_BUILD_LIBEV", "OFF");
+    build_cfg.define("LCB_BUILD_LIBUV", "OFF");
+
     let build_dst = build_cfg.build();
 
     println!(
