6db4fb971487a4dc26762ca75caf5b99d457eb29
Michael Nitschinger <michael@nitschinger.at>
2019-05-12 23:12:56 +0200

    fix doc versions

--- couchbase-sys/src/lib.rs
+++ couchbase-sys/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/couchbase-sys/0.4.0")]
+#![doc(html_root_url = "https://docs.rs/couchbase-sys/1.0.0-alpha.1")]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
--- couchbase/src/lib.rs
+++ couchbase/src/lib.rs
@@ -1,3 +1,5 @@
+#![doc(html_root_url = "https://docs.rs/couchbase/1.0.0-alpha.1")]
+
 mod bucket;
 mod cluster;
 mod collection;
