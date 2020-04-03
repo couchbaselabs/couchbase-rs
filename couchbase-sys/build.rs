634426cfdd99d00b385215a16b60c02b1e849005
Michael Nitschinger <michael@nitschinger.at>
2020-04-03 21:18:56 +0200

    add resolv for linux

--- couchbase-sys/build.rs
+++ couchbase-sys/build.rs
@@ -64,6 +64,7 @@ fn main() {
         if cfg!(any(target_os = "macos", target_os = "freebsd")) {
             println!("cargo:rustc-link-lib=dylib=c++");
         } else {
+            println!("cargo:rustc-link-lib=dylib=resolv");
             println!("cargo:rustc-link-lib=dylib=stdc++");
             println!("cargo:rustc-link-lib=dylib=gcc");
         }
