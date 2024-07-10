7b56a994e0e63142a69220f9f0cbd0c049805647
Mateusz <matt.wozakowski@gmail.com>
2024-07-10 14:35:03 +0100

    Protostellar initial commit

---
+++ sdk/protostellar/src/lib.rs
@@ -0,0 +1,77 @@
+pub mod couchbase {
+    pub mod admin {
+        pub mod analytics {
+            pub mod v1 {
+                include!("../genproto/couchbase.admin.analytics.v1.rs");
+            }
+        }
+        pub mod bucket {
+            pub mod v1 {
+                include!("../genproto/couchbase.admin.bucket.v1.rs");
+            }
+        }
+        pub mod collection {
+            pub mod v1 {
+                include!("../genproto/couchbase.admin.collection.v1.rs");
+            }
+        }
+        pub mod query {
+            pub mod v1 {
+                include!("../genproto/couchbase.admin.query.v1.rs");
+            }
+        }
+        pub mod search {
+            pub mod v1 {
+                include!("../genproto/couchbase.admin.search.v1.rs");
+            }
+        }
+    }
+    pub mod analytics {
+        pub mod v1 {
+            include!("../genproto/couchbase.analytics.v1.rs");
+        }
+    }
+    pub mod internal {
+        pub mod hooks {
+            pub mod v1 {
+                include!("../genproto/couchbase.internal.hooks.v1.rs");
+            }
+        }
+    }
+    pub mod kv {
+        pub mod v1 {
+            include!("../genproto/couchbase.kv.v1.rs");
+        }
+    }
+    pub mod query {
+        pub mod v1 {
+            include!("../genproto/couchbase.query.v1.rs");
+        }
+    }
+    pub mod routing {
+        pub mod v1 {
+            include!("../genproto/couchbase.routing.v1.rs");
+        }
+    }
+    pub mod search {
+        pub mod v1 {
+            include!("../genproto/couchbase.search.v1.rs");
+        }
+    }
+    pub mod transactions {
+        pub mod v1 {
+            include!("../genproto/couchbase.transactions.v1.rs");
+        }
+    }
+    pub mod view {
+        pub mod v1 {
+            include!("../genproto/couchbase.view.v1.rs");
+        }
+    }
+}
+
+pub mod google {
+    pub mod rpc {
+        include!("../genproto/google.rpc.rs");
+    }
+}
