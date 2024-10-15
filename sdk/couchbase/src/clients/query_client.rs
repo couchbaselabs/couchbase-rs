b99e2f2a93d3b9f55195a7539d57202e01de8ede
Charles Dixon <chvckd@gmail.com>
2024-10-15 13:44:07 +0100

    RSCBC-55: Implement scope level query

--- sdk/couchbase/src/clients/query_client.rs
+++ sdk/couchbase/src/clients/query_client.rs
@@ -34,13 +34,29 @@ pub(crate) enum QueryClientBackend {
     Couchbase2QueryClientBackend(Couchbase2QueryClient),
 }
 
+pub(crate) struct QueryKeyspace {
+    pub bucket_name: String,
+    pub scope_name: String,
+}
+
 pub(crate) struct CouchbaseQueryClient {
     agent: Agent,
+    keyspace: Option<QueryKeyspace>,
 }
 
 impl CouchbaseQueryClient {
     pub fn new(agent: Agent) -> Self {
-        Self { agent }
+        Self {
+            agent,
+            keyspace: None,
+        }
+    }
+
+    pub fn with_keyspace(agent: Agent, keyspace: QueryKeyspace) -> Self {
+        Self {
+            agent,
+            keyspace: Some(keyspace),
+        }
     }
 
     async fn query(
@@ -57,6 +73,14 @@ impl CouchbaseQueryClient {
 
         let mut query_opts = couchbase_core::queryoptions::QueryOptions::try_from(opts)?;
         query_opts.statement = Some(statement);
+
+        if let Some(keyspace) = &self.keyspace {
+            query_opts.query_context = Some(format!(
+                "{}.{}",
+                keyspace.bucket_name.clone(),
+                keyspace.scope_name.clone()
+            ));
+        }
 
         if ad_hoc {
             Ok(QueryResult::from(self.agent.query(query_opts).await?))
