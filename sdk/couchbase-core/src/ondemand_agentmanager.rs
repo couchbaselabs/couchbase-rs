d2b9845f3276bfb89ca004563466e264cada8d17
Charles Dixon <chvckd@gmail.com>
2024-12-12 10:55:47 +0000

    RSCBC-49: Update agent manager not to lock across agent creation

--- sdk/couchbase-core/src/ondemand_agentmanager.rs
+++ sdk/couchbase-core/src/ondemand_agentmanager.rs
@@ -3,7 +3,8 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use arc_swap::ArcSwap;
-use tokio::sync::Mutex;
+use log::debug;
+use tokio::sync::{Mutex, Notify};
 use typed_builder::TypedBuilder;
 
 use crate::agent::Agent;
@@ -68,6 +69,7 @@ pub struct OnDemandAgentManager {
     cluster_agent: Agent,
     fast_map: ArcSwap<HashMap<String, Agent>>,
     slow_map: Mutex<HashMap<String, Agent>>,
+    notif_map: Mutex<HashMap<String, Arc<Notify>>>,
 
     closed: std::sync::Mutex<bool>,
 }
@@ -81,6 +83,7 @@ impl OnDemandAgentManager {
             cluster_agent,
             fast_map: Default::default(),
             slow_map: Default::default(),
+            notif_map: Default::default(),
             closed: Default::default(),
         })
     }
@@ -119,17 +122,50 @@ impl OnDemandAgentManager {
 
     async fn get_bucket_agent_slow(&self, bucket_name: impl Into<String>) -> error::Result<Agent> {
         let bucket_name = bucket_name.into();
-        let mut slow_map = self.slow_map.lock().await;
-        if let Some(agent) = slow_map.get(&bucket_name) {
-            return Ok(agent.clone());
-        }
+        let notif = {
+            let mut slow_map = self.slow_map.lock().await;
+            if let Some(agent) = slow_map.get(&bucket_name) {
+                return Ok(agent.clone());
+            }
+
+            debug!(
+                "Bucket name {} not in slow map, checking notif map",
+                &bucket_name
+            );
+            // If we don't have an agent then check the notif map to see if someone else is getting
+            // an agent already. Note that we're still inside the slow_map lock here.
+            let mut notif_map = self.notif_map.lock().await;
+            if let Some(notif) = notif_map.get(&bucket_name) {
+                let notif = notif.clone();
+                drop(slow_map);
+                drop(notif_map);
+
+                debug!("Bucket name {} in notif map, awaiting update", &bucket_name);
+                notif.notified().await;
+                debug!("Bucket name {} received updated", &bucket_name);
+                return Box::pin(self.get_bucket_agent_slow(bucket_name)).await;
+            };
+
+            debug!("Bucket name {} not in any map, creating new", &bucket_name);
+            let notif = Arc::new(Notify::new());
+            notif_map.insert(bucket_name.clone(), notif.clone());
+
+            notif
+        };
 
         let mut opts: AgentOptions = self.opts.clone().into();
         opts.bucket_name = Some(bucket_name.clone());
 
         let agent = Agent::new(opts).await?;
 
-        slow_map.insert(bucket_name, agent.clone());
+        let mut slow_map = self.slow_map.lock().await;
+        slow_map.insert(bucket_name.clone(), agent.clone());
+
+        {
+            // We remove the entry from the notif map, whilst still under the slow_map lock.
+            let mut notif_map = self.notif_map.lock().await;
+            notif_map.remove(&bucket_name);
+        }
 
         let mut fast_map = HashMap::with_capacity(slow_map.len());
         for (name, agent) in slow_map.iter() {
@@ -137,6 +173,8 @@ impl OnDemandAgentManager {
         }
 
         self.fast_map.store(Arc::new(fast_map));
+
+        notif.notify_waiters();
 
         Ok(agent)
     }
