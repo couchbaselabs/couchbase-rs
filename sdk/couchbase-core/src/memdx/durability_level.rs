584a615d74d56dd85aa0129cf29bec7883734300
Charles Dixon <chvckd@gmail.com>
2025-03-19 09:08:34 +0000

    Update public durability level to use newtype pattern

--- sdk/couchbase-core/src/memdx/durability_level.rs
+++ sdk/couchbase-core/src/memdx/durability_level.rs
@@ -1,20 +1,49 @@
+use std::fmt::Display;
 use std::time::Duration;
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub enum DurabilityLevel {
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct DurabilityLevel(InnerDurabilityLevel);
+
+impl DurabilityLevel {
+    pub const MAJORITY: DurabilityLevel = DurabilityLevel(InnerDurabilityLevel::Majority);
+
+    pub const MAJORITY_AND_PERSIST_ACTIVE: DurabilityLevel =
+        DurabilityLevel(InnerDurabilityLevel::MajorityAndPersistActive);
+
+    pub const PERSIST_TO_MAJORITY: DurabilityLevel =
+        DurabilityLevel(InnerDurabilityLevel::PersistToMajority);
+
+    pub(crate) fn other(val: u8) -> DurabilityLevel {
+        DurabilityLevel(InnerDurabilityLevel::Other(val))
+    }
+}
+
+impl Display for DurabilityLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.0 {
+            InnerDurabilityLevel::Majority => write!(f, "majority"),
+            InnerDurabilityLevel::MajorityAndPersistActive => write!(f, "majorityAndPersistActive"),
+            InnerDurabilityLevel::PersistToMajority => write!(f, "persistToMajority"),
+            InnerDurabilityLevel::Other(val) => write!(f, "unknown({})", val),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub(crate) enum InnerDurabilityLevel {
     Majority,
-    MajorityAndPersistToActive,
+    MajorityAndPersistActive,
     PersistToMajority,
-    Unknown,
+    Other(u8),
 }
 
 impl From<DurabilityLevel> for u8 {
     fn from(value: DurabilityLevel) -> u8 {
         match value {
-            DurabilityLevel::Majority => 1,
-            DurabilityLevel::MajorityAndPersistToActive => 2,
-            DurabilityLevel::PersistToMajority => 3,
-            DurabilityLevel::Unknown => 0,
+            DurabilityLevel::MAJORITY => 1,
+            DurabilityLevel::MAJORITY_AND_PERSIST_ACTIVE => 2,
+            DurabilityLevel::PERSIST_TO_MAJORITY => 3,
+            _ => 0,
         }
     }
 }
@@ -22,10 +51,10 @@ impl From<DurabilityLevel> for u8 {
 impl From<u8> for DurabilityLevel {
     fn from(data: u8) -> Self {
         match data {
-            1 => DurabilityLevel::Majority,
-            2 => DurabilityLevel::MajorityAndPersistToActive,
-            3 => DurabilityLevel::PersistToMajority,
-            _ => DurabilityLevel::Unknown,
+            1 => DurabilityLevel::MAJORITY,
+            2 => DurabilityLevel::MAJORITY_AND_PERSIST_ACTIVE,
+            3 => DurabilityLevel::PERSIST_TO_MAJORITY,
+            _ => DurabilityLevel::other(data),
         }
     }
 }
