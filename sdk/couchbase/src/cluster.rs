3f7fc0bab43cf529fcbd664d24a944a32ba1f56c
Mateusz <matt.wozakowski@gmail.com>
2025-09-04 14:53:43 +0100

    RSCBC-148: Remove Cluster level Query and Search

--- sdk/couchbase/src/cluster.rs
+++ sdk/couchbase/src/cluster.rs
@@ -2,28 +2,19 @@ use crate::bucket::Bucket;
 use crate::clients::bucket_mgmt_client::BucketMgmtClient;
 use crate::clients::cluster_client::ClusterClient;
 use crate::clients::diagnostics_client::DiagnosticsClient;
-use crate::clients::query_client::QueryClient;
-use crate::clients::search_client::SearchClient;
 use crate::clients::user_mgmt_client::UserMgmtClient;
 use crate::error;
 use crate::management::buckets::bucket_manager::BucketManager;
 use crate::management::users::user_manager::UserManager;
 use crate::options::cluster_options::ClusterOptions;
 use crate::options::diagnostic_options::{DiagnosticsOptions, PingOptions, WaitUntilReadyOptions};
-use crate::options::query_options::QueryOptions;
-use crate::options::search_options::SearchOptions;
 use crate::results::diagnostics::{DiagnosticsResult, PingReport};
-use crate::results::query_results::QueryResult;
-use crate::results::search_results::SearchResult;
-use crate::search::request::SearchRequest;
 use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct Cluster {
     client: Arc<ClusterClient>,
     bucket_mgmt_client: Arc<BucketMgmtClient>,
-    query_client: Arc<QueryClient>,
-    search_client: Arc<SearchClient>,
     user_mgmt_client: Arc<UserMgmtClient>,
     diagnostics_client: Arc<DiagnosticsClient>,
 }
@@ -36,16 +27,12 @@ impl Cluster {
         let client = Arc::new(ClusterClient::connect(conn_str, opts).await?);
 
         let bucket_mgmt_client = Arc::new(client.buckets_client());
-        let query_client = Arc::new(client.query_client());
-        let search_client = Arc::new(client.search_client());
         let user_mgmt_client = Arc::new(client.users_client());
         let diagnostics_client = Arc::new(client.diagnostics_client());
 
         Ok(Cluster {
             client,
             bucket_mgmt_client,
-            query_client,
-            search_client,
             user_mgmt_client,
             diagnostics_client,
         })
@@ -63,25 +50,6 @@ impl Cluster {
 
     pub fn users(&self) -> UserManager {
         UserManager::new(self.user_mgmt_client.clone())
-    }
-
-    pub async fn query(
-        &self,
-        statement: impl Into<String>,
-        opts: impl Into<Option<QueryOptions>>,
-    ) -> error::Result<QueryResult> {
-        self.query_client.query(statement.into(), opts.into()).await
-    }
-
-    pub async fn search(
-        &self,
-        index_name: impl Into<String>,
-        request: SearchRequest,
-        opts: impl Into<Option<SearchOptions>>,
-    ) -> error::Result<SearchResult> {
-        self.search_client
-            .search(index_name.into(), request, opts.into())
-            .await
     }
 
     pub async fn diagnostics(
