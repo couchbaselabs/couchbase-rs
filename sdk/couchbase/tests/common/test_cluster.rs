fffe61366410b6fd243cb8b9da630bea50152361
Charles Dixon <chvckd@gmail.com>
2025-10-07 15:36:14 +0100

    RSCBC-206: Add copyright headers to files

--- sdk/couchbase/tests/common/test_cluster.rs
+++ sdk/couchbase/tests/common/test_cluster.rs
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * Copyright (c) 2025 Couchbase, Inc.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
 use crate::common::node_version::NodeVersion;
 use crate::common::test_bucket::TestBucket;
 use crate::common::test_config::TestSetupConfig;
@@ -5,10 +23,7 @@ use crate::common::test_manager::{TestBucketManager, TestUserManager};
 use couchbase::cluster::Cluster;
 use couchbase::error;
 use couchbase::options::query_options::QueryOptions;
-use couchbase::options::search_options::SearchOptions;
 use couchbase::results::query_results::QueryResult;
-use couchbase::results::search_results::SearchResult;
-use couchbase::search::request::SearchRequest;
 use std::ops::Deref;
 use std::time::Duration;
 use tokio::time::timeout;
