39480ff67ffe6c5ac9436bc3e914873a1c8d8ca9
Will Broadbelt <wbroadbelt@gmail.com>
2025-10-22 13:33:41 +0100

    RSCBC-72: Add Projections using Get
    Motivation:
    Add the Projections using Get feature, as defined in RFC #53.
    
    Changes:
    - Add projections Option to GetOptions
    - Do full doc Get or subdoc Lookup In depending on if projections
       and/or expiry is defined, and within lookup in limits (16).
    - Add test cases

--- sdk/couchbase/src/results/mod.rs
+++ sdk/couchbase/src/results/mod.rs
@@ -20,6 +20,7 @@ pub mod collections_mgmt_results;
 pub mod diagnostics;
 pub mod kv_binary_results;
 pub mod kv_results;
+pub(crate) mod projection;
 pub mod query_index_mgmt_results;
 pub mod query_results;
 pub mod search_results;
